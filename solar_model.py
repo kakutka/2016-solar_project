d72ecae75a05c4e3684a8c32c31a244796898ae0
Nailya21 <91528128+Nailya21@users.noreply.github.com>
2021-11-09 22:31:32 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -7,9 +7,7 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
@@ -18,33 +16,31 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += gravitation_constant*(body.x - obj.x)/r * body.m*obj.m/r**2 #+= 1  # FIXME: нужно вывести формулу...
-        body.Fy += gravitation_constant*(body.y - obj.y)/r * body.m*obj.m/r**2 #+= 2  # FIXME: нужно вывести формулу...
+        r_pow_2 = (body.x - obj.x)**2 + (body.y - obj.y)**2
+
+        body.Fx += body.m * obj.m / r_pow_2 * gravitational_constant * ((obj.x - body.x) / (r_pow_2**(0.5)))
+        body.Fy += body.m * obj.m / r_pow_2 * gravitational_constant * ((obj.y - body.y) / (r_pow_2**(0.5)))
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
-
+    
     ax = body.Fx/body.m
-    body.x += body.Vx*dt
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
     ay = body.Fy/body.m
-    body.y += body.Vy*dt # FIXME: не понимаю как менять...
-    body.Vx += ay*dt
+
+    body.x += body.Vx * dt
+    body.y += body.Vy * dt
+
+    body.Vx += ax*dt
+    body.Vy += ay*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
