7d5c7c16b52aba0449ef442e1f7c7750f71e09e2
Nailya21 <91528128+Nailya21@users.noreply.github.com>
2021-11-09 16:07:15 +0300

    Update solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -78,7 +78,7 @@ def create_planet_image(space, planet):
     r = planet.R
     planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
 
-    pass  # FIXME: сделать как у звезды
+    # FIXME: сделать как у звезды
 
 
 def update_system_name(space, system_name):
